4ef08da76a48fc056616a90aad38a97e4aaac5bb
jbastias <jorgebastias@hotmail.com>
2022-08-26 18:42:24 -0600

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import bootstrapLogo from '../assets/bootstrap-logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://getbootstrap.com" target="_blank">
+          <img
+            src={bootstrapLogo}
+            className="logo bootstrap"
+            alt="Bootstrap logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React + Bootstrap</h1>
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/components/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
